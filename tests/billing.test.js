655c04525fadbdd81284376e94a9bb7234943a41
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-31 00:15:50 +0200

    Added billing tests.

---
+++ tests/billing.test.js
@@ -0,0 +1,37 @@
+import { calculateCost } from "../libs/billing-lib";
+
+test("Free tier", () => {
+  const storage = 4;
+
+  const cost = 0;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Lowest tier", () => {
+  const storage = 10;
+
+  const cost = 4000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Middle tier", () => {
+  const storage = 100;
+
+  const cost = 20000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Highest tier", () => {
+  const storage = 101;
+
+  const cost = 10100;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
