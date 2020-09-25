0af32dac414f296cccc6863c860b08836dedaf8d
Petros Tsavdaridis <peteropon@gmail.com>
2020-09-25 11:39:21 +0200

    Modified listAll to return all active auctions.

--- listAll.js
+++ listAll.js
@@ -4,6 +4,8 @@ import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
+    KeyConditionExpression: "isActive = :isActive",
+    ExpressionAttributeValues: { ":isActive": true },
   };
 
   const result = await dynamoDb.query(params);
